9edcf45279de03212f6454efc928762420a70014
Hilary Lamb <hilaryklamb@gmail.com>
2020-01-06 21:59:47 -0500

    Updates

---
+++ precache-manifest.27996e2ace8c578c1c44c4745cfb382c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f87b1be27bc28ee3042c6b4b4b3c4a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "88599cb9cc1ac5bde9a9",
+    "url": "/clicky-game/static/css/main.0b64ff31.chunk.css"
+  },
+  {
+    "revision": "3bb6beffc8225f0f9d91",
+    "url": "/clicky-game/static/js/2.5f762d8b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.5f762d8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88599cb9cc1ac5bde9a9",
+    "url": "/clicky-game/static/js/main.9eb8085e.chunk.js"
+  },
+  {
+    "revision": "fb9aac71c9ddcd81e9bb",
+    "url": "/clicky-game/static/js/runtime-main.0958ddb3.js"
+  }
+]);
